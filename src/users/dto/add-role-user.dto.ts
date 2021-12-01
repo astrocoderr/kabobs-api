6d97c7b8420d77ce412c376746af7212a0c92eaa
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 07:55:38 +0500

    added class-validator on userID and roleID

--- src/users/dto/add-role-user.dto.ts
+++ src/users/dto/add-role-user.dto.ts
@@ -1,8 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber } from 'class-validator';
 
 export class AddRoleUserDto{
   @ApiProperty({ example: '23', description: "user's identifier" })
+  @IsNumber()
   readonly userID: number;
-  @ApiProperty({ example: '1', description: "role for user" })
+
+  @ApiProperty({ example: '1', description: 'role for user' })
+  @IsNumber()
   readonly roleID: number;
 }
