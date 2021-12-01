d6fa28c5453bae0ba91eca32211a6def3c2b6738
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:02:49 +0500

    added class-validator on userID and banReason

--- src/users/dto/ban-user.dto.ts
+++ src/users/dto/ban-user.dto.ts
@@ -1,8 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class BanUserDto{
   @ApiProperty({ example: '23', description: "user's identifier" })
+  @IsNumber()
   readonly userID: number;
+
   @ApiProperty({ example: 'for spam', description: 'user did some spams' })
+  @IsString()
   readonly banReason: string;
 }
