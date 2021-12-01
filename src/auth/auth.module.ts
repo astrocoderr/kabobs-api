31c256ba9af9000c7f94560f83aa959252746a7c
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:00:08 +0500

    jwt factory was moved into factory directory

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,25 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 
+import { jwtFactory } from './factories/jwt.factory';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
 import { RedisCacheModule } from '../redis/redis.module';
-import { RedisService } from "../redis/services/redis.service";
+
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV}.env`
-    }),
-    JwtModule.register({
-      secret: process.env.SECRET,
-      signOptions: {
-        expiresIn: Number(process.env.EXPIRES_IN)
-      }
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => jwtFactory(configService),
+      inject: [ConfigService]
     }),
     RedisCacheModule,
     UsersModule,
