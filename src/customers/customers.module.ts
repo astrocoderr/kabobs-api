0661d99236b2b1e2b0f380e21ab4fc7eba7c1164
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:10:53 +0500

    double quotes to single quotes

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -7,9 +7,9 @@ import { User } from '../users/models/user.model';
 import { Role } from '../roles/models/roles.model';
 import { UserRoles } from '../roles/models/user-roles.model';
 import { Customer } from './models/customer.model';
-import { Addresses } from "../addresses/models/addresses.model";
-import { CustomerAddresses } from "../addresses/models/customer-addresses.model";
-import { AddressesModule } from "../addresses/addresses.module";
+import { Addresses } from '../addresses/models/addresses.model';
+import { CustomerAddresses } from '../addresses/models/customer-addresses.model';
+import { AddressesModule } from '../addresses/addresses.module';
 
 @Module({
   controllers: [CustomersController],
