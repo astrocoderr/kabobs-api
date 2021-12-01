1a6ef0ecf143b34523e54fd87fe5e8fd2c0e9d6b
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 07:58:44 +0500

    added class-validator on email and password

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,8 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
 
 export class AuthDto{
   @ApiProperty({ example: 'john@john.com', description: 'email of user' })
+  @IsEmail()
   readonly email: string;
+
   @ApiProperty({ example: 'qwerty123', description: 'password of user' })
+  @IsString()
   readonly password: string;
 }
