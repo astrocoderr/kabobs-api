d066fe36829ef1404f40daed08abcb2837799e97
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:08:15 +0500

    added class-validator on name and description

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,8 +1,14 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateRoleDto{
   @ApiProperty({ example: 'user', description: "customer's role" })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(20)
   readonly name: string;
+
   @ApiProperty({ example: 'for customers', description: 'description for role' })
+  @IsString()
   readonly description: string;
 }
