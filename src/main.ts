d2d3eea771d236ba047559b26c38b908e39f856c
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:28:54 +0500

    added global app prefix and validation pipe, logger, configModule and configService

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,28 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { DocsConfig } from '../swagger-docs/swagger-docs.config';
 
-import { PORT } from '../config';
-import * as fs from "fs";
 
-
-async function start() {
+(async () => {
   const app = await NestFactory.create(AppModule, {   });
-
+  // Swagger Init
   DocsConfig(app)
 
-  await app.listen(PORT, () => {
-    console.log(`User-service started on port ${PORT}`)
+  const config = app.get(ConfigService)
+  app.setGlobalPrefix(config.get('GLOBAL_PREFIX'))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      disableErrorMessages: config.get('ENVIRONMENT') === config.get('PROD_ENV')
+    })
+  )
+  await app.listen(config.get('PORT'), () => {
+    Logger.log(`Listening at http://localhost:${config.get('PORT')}/${config.get('GLOBAL_PREFIX')}`)
+    Logger.log(`Running in ${config.get('ENVIRONMENT')} mode`)
   });
-}
-
-start();
+})()
