7d658510438108540baa6e13558bd193ed80ab08
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:22:00 +0500

    added logger context, added 'ms' into logger

--- utils/logger/logger.middleware.ts
+++ utils/logger/logger.middleware.ts
@@ -1,28 +1,23 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
-import * as fs from 'fs';
-
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  private logger = new Logger('HTTP');
+  private logger = new Logger('HTTP')
 
   use(request: Request, response: Response, next: NextFunction): void {
     const { ip, method, originalUrl } = request;
     const userAgent = request.get('user-agent') || '';
+    const now = Date.now()
 
     response.on('finish', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length');
 
       this.logger.log(
-        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} +${Date.now()-now}ms`,
       );
-
-      if(method != 'GET'){
-        this.logger.error(request.body)
-      }
     });
 
     next();
