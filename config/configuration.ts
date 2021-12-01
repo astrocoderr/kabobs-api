d21fc2c5c7114733ac9cdc64fa87123128de59c3
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:04:42 +0500

    created configuration.ts file for api configs

---
+++ config/configuration.ts
@@ -0,0 +1,61 @@
+export const configuration = () => {
+  return {
+    // application credentials
+    ENVIRONMENT: `.${process.env.NODE_ENV}.env`,
+    PORT: process.env.PORT,
+    GLOBAL_PREFIX: process.env.GLOBAL_PREFIX,
+
+    // database
+    DB: {
+      DIALECT: process.env.DB_DIALECT,
+      HOST: process.env.DB_HOST,
+      NAME: process.env.DB_NAME,
+      PORT: process.env.DB_PORT,
+      USER: process.env.DB_USER,
+      PASS: process.env.DB_PASS,
+      AUTOLOADMODELS: process.env.DB_AUTOLOAD_MODELS
+    },
+
+    // json web token
+    JWT: {
+      SECRET: process.env.JWT_SECRET,
+      EXPIRES_IN: process.env.JWT_EXPIRES_IN
+    },
+
+    // bcrypt
+    BCRYPT_SALT: process.env.BCRYPT_SALT,
+
+    // redis
+    REDIS: {
+      HOST: process.env.REDIS_HOST,
+      PORT: process.env.REDIS_PORT
+    },
+
+    // loggerFactory
+    LOGGER: {
+      META: process.env.LOGGER_META,
+      APPNAME: process.env.LOGGER_APPNAME,
+      INFO: {
+        LEVEL: process.env.LOGGER_INFO_LEVEL,
+        FILE: {
+          PATH: process.env.LOGGER_INFO_FILE_PATH
+        }
+      },
+      WARN: {
+        LEVEL: process.env.LOGGER_WARN_LEVEL,
+        FILE: {
+          PATH: process.env.LOGGER_WARN_FILE_PATH
+        }
+      },
+      ERROR: {
+        LEVEL: process.env.LOGGER_ERROR_LEVEL,
+        FILE: {
+          PATH: process.env.LOGGER_ERROR_FILE_PATH
+        }
+      }
+    },
+
+    // other params
+    PROD_ENV: process.env.PROD_ENV
+  }
+}
