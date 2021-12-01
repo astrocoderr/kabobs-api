25c5dba1a7a1952717486f566455fb1b05af8614
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-01 08:24:46 +0500

    added class-validator on userID and unbanReason

--- src/users/dto/unban-user.dto.ts
+++ src/users/dto/unban-user.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from 'class-validator';
 
 export class UnbanUserDto{
   @ApiProperty({ example: '23', description: "user's identifier" })
+  @IsNumber()
   readonly userID: number;
+
   @ApiProperty({ example: 'paid commissions', description: 'user paid commissions' })
+  @IsString()
   readonly unbanReason: string;
 }
