7038068b06f0b1ceebe4900cce83ae35f09ef7bc
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-02 10:29:39 +0500

    imported AuthModule

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -6,9 +6,10 @@ import { RolesController } from './controllers/roles.controller';
 import { Role } from './models/roles.model';
 import { User } from '../users/models/user.model';
 import { UserRoles } from './models/user-roles.model';
-import { Customer } from "../customers/models/customer.model";
-import { Addresses } from "../addresses/models/addresses.model";
-import { CustomerAddresses } from "../addresses/models/customer-addresses.model";
+import { Customer } from '../customers/models/customer.model';
+import { Addresses } from '../addresses/models/addresses.model';
+import { CustomerAddresses } from '../addresses/models/customer-addresses.model';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   providers: [RolesService],
@@ -17,7 +18,8 @@ import { CustomerAddresses } from "../addresses/models/customer-addresses.model"
     SequelizeModule.forFeature([
       Role, User, UserRoles, Customer,
       Addresses, CustomerAddresses
-    ])
+    ]),
+    AuthModule
   ],
   exports: [
     RolesService
