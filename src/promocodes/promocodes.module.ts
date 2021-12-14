325a810123108fcb80e2cebbf73f9b6970dda6a7
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-14 09:59:18 +0500

    created promocodes module, added sequalize modules

---
+++ src/promocodes/promocodes.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { PromocodesController } from './controllers/promocodes.controller';
+import { PromocodesService } from './services/promocodes.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from '../users/models/user.model';
+import { Role } from '../roles/models/roles.model';
+import { UserRoles } from '../roles/models/user-roles.model';
+import { Customer } from '../customers/models/customers.model';
+import { Addresses } from '../addresses/models/addresses.model';
+import { CustomerAddresses } from '../addresses/models/customer-addresses.model';
+import { Promocode } from './models/promocodes.model';
+import { AuthModule } from '../auth/auth.module';
+import { Order } from '../orders/models/orders.model';
+import { Kitchen } from '../kitchens/models/kitchens.model';
+import { KitchenUser } from '../kitchens/models/kitchen-users.model';
+
+@Module({
+  controllers: [PromocodesController],
+  providers: [PromocodesService],
+  imports: [
+    SequelizeModule.forFeature([
+      User, Role, UserRoles, Customer,
+      Addresses, CustomerAddresses, Promocode,
+      Order, Kitchen, KitchenUser
+    ]),
+    AuthModule
+  ]
+
+})
+export class PromocodesModule {}
