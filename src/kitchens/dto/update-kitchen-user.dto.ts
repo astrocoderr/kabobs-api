ce3be0e73fdf9b1c408e3d15669c486dcb1f047a
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-14 10:05:05 +0500

    created update kitchen user dto, added validation and swagger

---
+++ src/kitchens/dto/update-kitchen-user.dto.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail, IsNumber, IsString, Matches, MaxLength,
+  MinLength
+} from 'class-validator';
+
+export class UpdateKitchenUserDto {
+  @ApiProperty({ example: 'John', description: 'first name' })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  firstName: string;
+
+  @ApiProperty({ example: 'Mathew', description: 'last name' })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  lastName: string;
+
+  @ApiProperty({ example: 'jackandjones@gmail.com', description: 'email address' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'qwerty1234', description: 'password' })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(50)
+  @Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    { message: 'password too weak' }
+  )
+  password: string;
+
+  @ApiProperty({ example: '68', description: 'kitchens identifier' })
+  @IsNumber()
+  kitchen: number;
+}
