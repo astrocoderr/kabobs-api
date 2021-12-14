15b4eeb483571a09a48eabe3a01c799f6de5f031
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-14 09:36:29 +0500

    added promocode, order, kitchen and kitchen user modules

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -6,11 +6,15 @@ import { CustomersService } from './services/customers.service';
 import { User } from '../users/models/user.model';
 import { Role } from '../roles/models/roles.model';
 import { UserRoles } from '../roles/models/user-roles.model';
-import { Customer } from './models/customer.model';
+import { Customer } from './models/customers.model';
 import { Addresses } from '../addresses/models/addresses.model';
 import { CustomerAddresses } from '../addresses/models/customer-addresses.model';
 import { AddressesModule } from '../addresses/addresses.module';
 import { AuthModule } from '../auth/auth.module';
+import { Promocode } from '../promocodes/models/promocodes.model';
+import { Order } from '../orders/models/orders.model';
+import { Kitchen } from '../kitchens/models/kitchens.model';
+import { KitchenUser } from '../kitchens/models/kitchen-users.model';
 
 @Module({
   controllers: [CustomersController],
@@ -18,7 +22,8 @@ import { AuthModule } from '../auth/auth.module';
   imports: [
     SequelizeModule.forFeature([
       User, Role, UserRoles, Customer,
-      Addresses, CustomerAddresses
+      Addresses, CustomerAddresses, Promocode,
+      Order, Kitchen, KitchenUser
     ]),
     AddressesModule,
     AuthModule
