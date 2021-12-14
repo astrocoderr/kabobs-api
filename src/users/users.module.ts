610ebd2b9c21ea0bc22a51f24968ba5659bb4c9d
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-14 09:39:14 +0500

    added promocode, order, kitchen and kitchen users modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,10 +7,14 @@ import { User } from './models/user.model';
 import { Role } from '../roles/models/roles.model';
 import { UserRoles } from '../roles/models/user-roles.model';
 import { RolesModule } from '../roles/roles.module';
-import { Customer } from '../customers/models/customer.model';
+import { Customer } from '../customers/models/customers.model';
 import { Addresses } from '../addresses/models/addresses.model';
 import { CustomerAddresses } from '../addresses/models/customer-addresses.model';
 import { AuthModule } from '../auth/auth.module';
+import { Promocode } from '../promocodes/models/promocodes.model';
+import { Order } from '../orders/models/orders.model';
+import { Kitchen } from '../kitchens/models/kitchens.model';
+import { KitchenUser } from '../kitchens/models/kitchen-users.model';
 
 @Module({
   controllers: [UsersController],
@@ -18,7 +22,8 @@ import { AuthModule } from '../auth/auth.module';
   imports: [
     SequelizeModule.forFeature([
       User, Role, UserRoles, Customer,
-      Addresses, CustomerAddresses
+      Addresses, CustomerAddresses, Promocode,
+      Order, Kitchen, KitchenUser
     ]),
     RolesModule,
     forwardRef(() => AuthModule)
