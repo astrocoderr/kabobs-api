b2c269242efce831c67438f07717ef80175c01e4
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-24 10:46:47 +0500

    created create group ingredient dto, added validator and swagger

---
+++ src/group-ingredients/dto/create-group-ingredient.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean, IsString, MaxLength, MinLength
+} from 'class-validator';
+
+export class CreateGroupIngredientDto {
+  @ApiProperty({ example: 'fruits', description: 'group ingredient name' })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  name: string;
+
+  @ApiProperty({ example: true, description: 'is group-ingredients is active' })
+  @IsBoolean()
+  active: boolean;
+}
