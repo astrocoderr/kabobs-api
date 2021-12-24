b75b40e59b05de5fd740414e97361227c81d6944
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-24 10:48:04 +0500

    created search user dto, added validator and swagger

---
+++ src/users/dto/search-user.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString
+} from 'class-validator';
+
+export class SearchUserDto {
+  @ApiProperty({ example: "'45'", description: 'parameter that goes on searching' })
+  @IsString()
+  search: string;
+}
