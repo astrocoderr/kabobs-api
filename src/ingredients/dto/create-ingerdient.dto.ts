465d622b52bf46a93e7de9e44ec35a96d0226545
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-24 10:47:04 +0500

    created create ingredient dto, added validator and swagger

---
+++ src/ingredients/dto/create-ingerdient.dto.ts
@@ -0,0 +1,57 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean, IsNumber, IsString, MaxLength, MinLength
+} from 'class-validator';
+
+
+export class CreateIngerdientDto {
+  @ApiProperty({ example: 'apple', description: 'ingredient name' })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  name: string;
+
+  @ApiProperty({ example: '200', description: 'kcal' })
+  @IsNumber()
+  kcal: number;
+
+  @ApiProperty({ example: '100', description: 'protein' })
+  @IsNumber()
+  prot: number;
+
+  @ApiProperty({ example: '40', description: 'fat' })
+  @IsNumber()
+  fat: number;
+
+  @ApiProperty({ example: '68', description: 'carb' })
+  @IsNumber()
+  carb: number;
+
+  @ApiProperty({ example: '33', description: "group's identifier" })
+  @IsNumber()
+  group: number;
+
+  @ApiProperty({ example: 'unit', description: "unit" })
+  @IsString()
+  unit: string;
+
+  @ApiProperty({ example: '500', description: 'brutto' })
+  @IsNumber()
+  brutto: number;
+
+  @ApiProperty({ example: '250', description: 'netto' })
+  @IsNumber()
+  netto: number;
+
+  @ApiProperty({ example: '5', description: 'percent' })
+  @IsNumber()
+  percent: number;
+
+  @ApiProperty({ example: 'apple, pineapple', description: 'tags; (apple, pineaplle)' })
+  @IsString()
+  tags: string;
+
+  @ApiProperty({ example: true, description: 'is orders is active' })
+  @IsBoolean()
+  active: boolean;
+}
