0d8f2e7a600b5068d062b236c228d946bba3a8d2
alishersulimov@gmail.com <alishersulimov@gmail.com>
2021-12-24 10:55:03 +0500

    created ingredients module, imported sequelize module and custom modules

---
+++ src/ingredients/ingredients.module.ts
@@ -0,0 +1,40 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { IngredientsController } from './controllers/ingredients.controller';
+import { IngredientsService } from './services/ingredients.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from '../users/models/user.model';
+import { Role } from '../roles/models/roles.model';
+import { UserRoles } from '../roles/models/user-roles.model';
+import { Customer } from '../customers/models/customers.model';
+import { Addresses } from '../addresses/models/addresses.model';
+import { CustomerAddresses } from '../addresses/models/customer-addresses.model';
+import { Promocode } from '../promocodes/models/promocodes.model';
+import { Order } from '../orders/models/orders.model';
+import { Kitchen } from '../kitchens/models/kitchens.model';
+import { KitchenUser } from '../kitchens/models/kitchen-users.model';
+import { OrderDays } from '../order-days/models/order-days.model';
+import { GroupIngredient } from '../group-ingredients/models/group-ingredients.model';
+import { Ingredient } from './models/ingredients.model';
+import { GroupIngredientsModule } from '../group-ingredients/group-ingredients.module';
+import { AuthModule } from '../auth/auth.module';
+import { Techcard } from '../techcards/models/techcards.model';
+
+@Module({
+  controllers: [IngredientsController],
+  providers: [IngredientsService],
+  imports: [
+    SequelizeModule.forFeature([
+      User, Role, UserRoles, Customer,
+      Addresses, CustomerAddresses, Promocode,
+      Order, Kitchen, KitchenUser, OrderDays,
+      GroupIngredient, Ingredient, Techcard
+    ]),
+    AuthModule,
+    forwardRef(() => GroupIngredientsModule)
+  ],
+  exports: [
+    IngredientsModule,
+    IngredientsService
+  ]
+})
+export class IngredientsModule {}
