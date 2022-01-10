31215eb652352b17f850951e8377434ef625dec5
alishersulimov@gmail.com <alishersulimov@gmail.com>
2022-01-10 09:11:03 +0500

    file prettified

--- src/group-ingredients/dto/create-group-ingredient.dto.ts
+++ src/group-ingredients/dto/create-group-ingredient.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import {
-  IsBoolean, IsString, MaxLength, MinLength
+  IsString, MaxLength, MinLength
 } from 'class-validator';
 
 export class CreateGroupIngredientDto {
@@ -9,8 +9,4 @@ export class CreateGroupIngredientDto {
   @MinLength(1)
   @MaxLength(50)
   name: string;
-
-  @ApiProperty({ example: true, description: 'is group-ingredients is active' })
-  @IsBoolean()
-  active: boolean;
 }
