ccf35dd72ac84605c8e2f95426593aedce495674
alishersulimov@gmail.com <alishersulimov@gmail.com>
2022-01-11 10:53:20 +0500

    created manager_orders_association, table, and added columns

---
+++ src/orders/models/manager-orders-associations.model.ts
@@ -0,0 +1,27 @@
+import {
+  Column, DataType, ForeignKey,
+  Model, Table
+} from 'sequelize-typescript';
+
+import { User } from '../../users/models/user.model';
+import { Order } from './orders.model';
+
+// manager_orders_associations model
+@Table({ tableName: 'manager_orders_associations' })
+export class ManagerOrdersAssociations extends Model<ManagerOrdersAssociations>{
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true
+  })
+  id: number;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.INTEGER })
+  managerID: number;
+
+  @ForeignKey(() => Order)
+  @Column({ type: DataType.INTEGER })
+  orderID: number;
+}
