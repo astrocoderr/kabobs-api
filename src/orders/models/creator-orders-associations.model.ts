615e520578e73866a004d304b504155e216d3008
alishersulimov@gmail.com <alishersulimov@gmail.com>
2022-01-11 10:52:03 +0500

    created creator_orders_association and table

---
+++ src/orders/models/creator-orders-associations.model.ts
@@ -0,0 +1,27 @@
+import {
+  Column, DataType, ForeignKey,
+  Model, Table
+} from 'sequelize-typescript';
+
+import { User } from '../../users/models/user.model';
+import { Order } from './orders.model';
+
+// creator_orders_associations model
+@Table({ tableName: 'creator_orders_associations' })
+export class CreatorOrdersAssociations extends Model<CreatorOrdersAssociations>{
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true
+  })
+  id: number;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.INTEGER })
+  creatorID: number;
+
+  @ForeignKey(() => Order)
+  @Column({ type: DataType.INTEGER })
+  orderID: number;
+}
