83e13b9c38188505eecaa1dea7ec52a17051226a
alishersulimov@gmail.com <alishersulimov@gmail.com>
2022-01-11 10:45:11 +0500

    changed userID to managerID and added association

--- src/customers/models/customers.model.ts
+++ src/customers/models/customers.model.ts
@@ -1,6 +1,6 @@
 import {
   BelongsToMany, Column,
-  DataType, Model, Table
+  DataType, ForeignKey, HasOne, Model, Table,
 } from 'sequelize-typescript';
 import { ApiProperty } from '@nestjs/swagger';
 
@@ -8,6 +8,12 @@ import { Addresses } from '../../addresses/models/addresses.model';
 import {
   CustomerAddressesAssociations,
 } from '../../addresses/models/customer-addresses-associations.model';
+import { Promocode } from '../../promocodes/models/promocodes.model';
+import {
+  CustomerPromocodesAssociations
+} from '../../promocodes/models/customer-promocodes-associations.model';
+import { User } from '../../users/models/user.model';
+import { UserCustomersAssociations } from './user-customers-associations.model';
 
 
 // customer creation attributes
@@ -24,7 +30,7 @@ interface CustomerFields {
   kitchenID: number;
   isVIP: boolean;
   language: string;
-  userID: number;
+  managerID: number;
   status: boolean;
   password: string;
 }
@@ -116,9 +122,35 @@ export class Customer extends Model<Customer, CustomerFields>{
   @Column({ type: DataType.STRING, defaultValue: 'en' })
   language: string;
 
-  @ApiProperty({ example: '1', description: 'manager identifier' })
-  @Column({ type: DataType.INTEGER, allowNull: false })
-  userID: number;
+  @ApiProperty({
+    example: {
+      "id": 2,
+      "text": "Delaware, St. Riston 1A-22",
+      "lat": 12.345678,
+      "lon": 23.456798,
+      "road": "Avenue, 1C",
+      "houseNumber": "42a BBC",
+      "neighbourhood": "smth",
+      "zipcode": 12345,
+      "active": true,
+      "createdAt": "2021-11-12T06:15:55.612Z",
+      "updatedAt": "2021-11-12T06:15:55.612Z",
+      "CustomerAddresses": {
+        "id": 2,
+        "addressID": 2,
+        "customerID": 7,
+        "createdAt": "2021-11-12T06:20:56.582Z",
+        "updatedAt": "2021-11-12T06:20:56.582Z"
+      }
+    },
+    type: 'object',
+    description: 'role identifier'
+  })
+  @BelongsToMany(() => User, () => UserCustomersAssociations)
+  manager: number;
+
+  @ForeignKey(() => User)
+  managerID: number
 
   @ApiProperty({ example: true, description: 'is customer is active' })
   @Column({ type: DataType.BOOLEAN, defaultValue: true })
@@ -146,4 +178,31 @@ export class Customer extends Model<Customer, CustomerFields>{
   @ApiProperty({ example: 'true', description: 'user uses 2 factor authentication' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   twoFA: boolean;
+
+  @ApiProperty({
+    example: {
+      "id": 2,
+      "text": "Delaware, St. Riston 1A-22",
+      "lat": 12.345678,
+      "lon": 23.456798,
+      "road": "Avenue, 1C",
+      "houseNumber": "42a BBC",
+      "neighbourhood": "smth",
+      "zipcode": 12345,
+      "active": true,
+      "createdAt": "2021-11-12T06:15:55.612Z",
+      "updatedAt": "2021-11-12T06:15:55.612Z",
+      "CustomerAddresses": {
+        "id": 2,
+        "addressID": 2,
+        "customerID": 7,
+        "createdAt": "2021-11-12T06:20:56.582Z",
+        "updatedAt": "2021-11-12T06:20:56.582Z"
+      }
+    },
+    type: 'object',
+    description: 'role identifier'
+  })
+  @BelongsToMany(() => Promocode, () => CustomerPromocodesAssociations)
+  promocode: number;
 }
