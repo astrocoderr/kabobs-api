b8d792edc460b4b89fef6524271502c222e53e30
alishersulimov@gmail.com <alishersulimov@gmail.com>
2022-01-19 00:39:16 +0500

    added global filter HttpException

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,36 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { DocsConfig } from '../swagger-docs/swagger-docs.config';
+import { HttpExceptionFilter } from '../filters/http-exception.filter';
 
 
 (async () => {
   const app = await NestFactory.create(AppModule, {   });
-  // Swagger Init
-  DocsConfig(app)
+  // swagger init
+  DocsConfig(app);
 
+  // config adding
   const config = app.get(ConfigService)
-  app.setGlobalPrefix(config.get('GLOBAL_PREFIX'))
+  app.setGlobalPrefix(config.get('GLOBAL_PREFIX'));
+
+  // filter init
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // validation init
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
+
       forbidNonWhitelisted: true,
       disableErrorMessages: config.get('ENVIRONMENT') === config.get('PROD_ENV')
     })
-  )
+  );
+
+  // CORS
   app.enableCors({ origin: [ `http://localhost:${config.get('PORT')}` ] });
+
+  // server start
   await app.listen(config.get('PORT'), () => {
     Logger.log(`Listening at http://localhost:${config.get('PORT')}/${config.get('GLOBAL_PREFIX')}`)
     Logger.log(`Running in ${config.get('ENVIRONMENT')} mode`)
