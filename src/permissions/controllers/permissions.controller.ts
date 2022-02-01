1618ba9be909e24f80b1ce7964a0e99570c45328
alishersulimov@gmail.com <alishersulimov@gmail.com>
2022-02-01 17:02:05 +0500

    removed api description response

--- src/permissions/controllers/permissions.controller.ts
+++ src/permissions/controllers/permissions.controller.ts
@@ -2,10 +2,9 @@ import {
   Body, Controller, Delete, Get, Param,
   Post, Put, UseGuards
 } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
-import { Promocode } from '../../promocodes/models/promocodes.model';
 import { PermissionsService } from '../services/permissions.service';
 import { CreatePermissionDto } from '../dto/create-permission.dto';
 import { UpdatePermissionDto } from '../dto/update-permission.dto';
@@ -19,35 +18,30 @@ export class PermissionsController {
 
   // Permission CRUD
   @ApiOperation({ summary: 'Creating a permission' })
-  @ApiResponse({ status: 200, type: Promocode })
   @Post()
   createPermission(@Body()  dto: CreatePermissionDto){
     return this.permissionsService.createPermission(dto)
   }
 
   @ApiOperation({ summary: 'Getting a permission' })
-  @ApiResponse({ status: 200, type: Promocode })
   @Get('/:name')
   getPermission(@Param('name') name: string){
     return this.permissionsService.getPermission(name)
   }
 
   @ApiOperation({ summary: 'Getting permission' })
-  @ApiResponse({ status: 200, type: [Promocode] })
   @Get()
   getPermissions(){
     return this.permissionsService.getPermissions()
   }
 
   @ApiOperation({ summary: 'Modifying a permission' })
-  @ApiResponse({ status: 200, type: Promocode })
   @Put('/:name')
   modifyPermission(@Param('name') name: string, @Body() dto: UpdatePermissionDto){
     return this.permissionsService.modifyPermission(name, dto)
   }
 
   @ApiOperation({ summary: 'Removing a permission' })
-  @ApiResponse({ status: 200, type: Promocode })
   @Delete('/:name')
   removePermission(@Param('name') name: string){
     return this.permissionsService.removePermission(name)
