46db2905f8d8bafc023fc67656e8161a77fbca3e
alishersulimov@gmail.com <alishersulimov@gmail.com>
2022-02-08 17:45:05 +0500

    added order days association and week_size parameter

--- src/orders/models/orders.model.ts
+++ src/orders/models/orders.model.ts
@@ -10,6 +10,7 @@ import { Customer } from '../../customers/models/customers.model';
 import { ManagerOrdersAssociations } from './manager-orders-associations.model';
 import { CreatorOrdersAssociations } from './creator-orders-associations.model';
 import { CustomerOrdersAssociations } from './customer-orders-associations';
+import { OrderDays } from '../../order-days/models/order-days.model';
 
 
 // orders creation attributes
@@ -201,18 +202,18 @@ export class Order extends Model<Order, OrderFields>{
   length: number;
 
   @ApiProperty({
-    example: '2',
-    description: 'meals per day, f.i. 2 means in the morning and in the evening'
-  })
-  @Column({ type: DataType.INTEGER, allowNull: false })
-  meals_per_day: number;
-
-  @ApiProperty({
     example: '3',
     description: 'week size, f.i. 3 means 3 days continuously, days starts from monday'
   })
   @Column({ type: DataType.INTEGER, allowNull: false })
   week_size: number;
+
+  @ApiProperty({
+    example: '2',
+    description: 'meals per day, f.i. 2 means in the morning and in the evening'
+  })
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  meals_per_day: number;
 
   @ApiProperty({ example: '500', description: '500 czech koruna' })
   @Column({ type: DataType.INTEGER, allowNull: false })
@@ -249,4 +250,34 @@ export class Order extends Model<Order, OrderFields>{
   @ApiProperty({ example: 1, description: 'web' })
   @Column({ type: DataType.INTEGER, allowNull: false })
   source: number;
+
+  @ApiProperty({
+    example: {
+      "id": 2,
+      "text": "Delaware, St. Riston 1A-22",
+      "lat": 12.345678,
+      "lon": 23.456798,
+      "road": "Avenue, 1C",
+      "houseNumber": "42a BBC",
+      "neighbourhood": "smth",
+      "zipcode": 12345,
+      "active": true,
+      "createdAt": "2021-11-12T06:15:55.612Z",
+      "updatedAt": "2021-11-12T06:15:55.612Z",
+      "CustomerAddresses": {
+        "id": 2,
+        "addressID": 2,
+        "customerID": 7,
+        "createdAt": "2021-11-12T06:20:56.582Z",
+        "updatedAt": "2021-11-12T06:20:56.582Z"
+      }
+    },
+    type: 'object',
+    description: 'role identifier'
+  })
+  @HasMany(() => OrderDays)
+  order_day: number;
+
+  @ForeignKey(() => OrderDays)
+  order_day_id: number
 }
